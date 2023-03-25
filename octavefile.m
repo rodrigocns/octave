d4057022ba4339203044014cd40c7c962367e2fe
rodrigocns <rodrigoconsoli@gmail.com>
2023-03-25 12:55:35 -0300

    Update octavefile.m

--- octavefile.m
+++ octavefile.m
@@ -1,11 +1,26 @@
 pkg load io
-#{ 
-#json method
-json_string = fileread('OctaveAccessTest.json');
-data_obj = fromJSON(json_string);
-#}
+#sheets_filename = 'Octave32767str.xlsx';
+sheets_filename = 'iRT gsheets.xlsx';
+col_to_decompress = [6,7,8,9,10,11]; 
+
 tic;
-sheets_filename = 'Octave32767str.xlsx';
-toc;
 [sheets_num,sheets_txt,sheets_raw] = xlsread(sheets_filename,1);
-split(sheets_raw[],",");
+toc;
+
+function data_ready = decompress_data(compressed_data)
+  splitted_data = strsplit(compressed_data, ',');
+  data_ready = str2double(splitted_data);
+endfunction
+
+
+
+teste = decompress_data(sheets_raw{2,3});
+teste
+#{
+sheets_raw
+
+sheets_raw{:,1}
+
+
+
+#}
