eec73e3362a36ba3e35d679870787f6076194695
rodrigocns <rodrigoconsoli@gmail.com>
2023-04-19 15:44:18 -0300

    Update 1.uncompressing_sheets.m
    Change variable names
    Add new sheet page for session/task data
    Update sections order: Configs, functions, scripts

--- 1.uncompressing_sheets.m
+++ 1.uncompressing_sheets.m
@@ -1,19 +1,21 @@
 pkg load io
 %Script lê arquivo 'input_filename' e cria um arquivo csv 'output_filename'
-% expandindo de uma resolução por linha para um ponto de dado por linha.
+% expandindo de uma resolução por i_line para um ponto de dado por i_line.
 
 clear
+% Settings (altere conforme necessário) ====================
 input_filename = 'iRT gsheets.xlsx'; %downloaded sheets filename (format incl.).
 output_filename = 'iRT_data'; %sem o formato de arquivo
-colunas = [8,9,10,11,12,13]; %colunas do input para descomprimir
-
-% Funções =================
-function data_ready = extract(compressed_data)
+zipped_rows = [8,9,10,11,12,13]; %colunas do input para descomprimir
+header={"sessionID", "task_id", "epoch", "duration", "Qi", "Qj", "Qk", "Qr"};
+header_atemporal={"sessionID", "task_id", "startEpoch", "email", "subject", "pxAngsRatio"};
+% Funções =================================================
+function uncompressed_data = extract(compressed_data)
   splitted_data = strsplit(compressed_data, ',');
-  data_ready = str2double(splitted_data)';
+  uncompressed_data = str2double(splitted_data)';
 endfunction
 
-% Copiar dados da planilha ============================
+% Copiar dados da planilha para o octave =================
 tic;printf("Lendo planilha...");
 [sheets_num,sheets_txt,sheets_raw] = xlsread(input_filename,1);
 printf("DONE! ");
@@ -21,28 +23,39 @@ toc;
 
 % Descompactando dados ===================================
 tic;printf("Descomprimindo dados...");
-header={"sessionID", "task_id", "epoch", "duration", "Qi", "Qj", "Qk", "Qr"};
-full_matrix = header;
 
-%para cada linha de dados (2a até a ultima)
+full_matrix = header;
+atemporal_cmatrix = header_atemporal;
+%para cada linha de dados compactos (2a até a ultima)
 contador = 0;
-for linha = 2:size(sheets_raw,1)
-  numeric_matrix = [];
-  %para cada coluna de dados compact.
-  for col_id = 1:size(colunas,2);
-    %descompactar dados das celulas em uma matriz inteira
-    numeric_matrix = [numeric_matrix,extract(sheets_raw{linha,colunas(col_id)})];
+for i_line = 2:size(sheets_raw,1)
+  % pega dados únicos da sessão/task (session_id, task_id, etc)
+  session_id = sheets_raw{i_line,4};
+  task_id = sheets_raw{i_line,5};
+  pxAngsRatio = mean(extract(sheets_raw{i_line,6}));
+
+  % descompactar cada array de dados na matriz temporal_array (de uma para várias linhas)
+  % uma coluna por vez
+  temporal_array = [];
+  for i_col = 1:size(zipped_rows,2)
+    temporal_array = [temporal_array, extract( sheets_raw{ i_line, zipped_rows(i_col) }) ];
   endfor
-  %soma epoch inicial na coluna de epoch
-  numeric_matrix(:,1) = numeric_matrix(:,1) + str2num(sheets_raw{linha,7});
-  %criando uma cell matrix para os dados da task
-  session_id = sheets_raw{linha,4};
-  task_id = sheets_raw{linha,5};
-  cell_matrix = cell(size(numeric_matrix,1),2);  %cria cell matrix n x 2
-  cell_matrix(:,1) = {session_id};  %preenche IDs
-  cell_matrix(:,2) = {task_id};
-  task_matrix = horzcat(cell_matrix,num2cell(numeric_matrix));
-  full_matrix = vertcat(full_matrix,task_matrix);
+
+  % epoch_n recebe epoch_0 + delta (linha i_line, coluna 7)
+  temporal_array(:,1) = temporal_array(:,1) + str2num(sheets_raw{i_line,7});
+
+  % cria cell matrix n x 2, n -> comprimento de temporal_array
+  task_cmatrix = cell(size(temporal_array,1),2);
+  task_cmatrix(:,1) = {session_id};  % preenche IDs
+  task_cmatrix(:,2) = {task_id};
+  task_cmatrix = horzcat(task_cmatrix,num2cell(temporal_array));
+
+  % append na matriz completa
+  full_matrix = vertcat(full_matrix,task_cmatrix);
+
+  % append na matriz de dados constantes da sessao
+  atemporal_cmatrix = vertcat(atemporal_cmatrix, {session_id, task_id, sheets_raw{i_line,7}, sheets_raw{i_line,2}, sheets_raw{i_line,3}, pxAngsRatio });
+
   contador++;
 endfor
 printf("%i linhas processadas. DONE! ",contador);toc;
@@ -51,15 +64,10 @@ clear contador;
 % Salvando os dados ====================================
 tic;printf("Salvando dados...");
 
-
-
-% Write to XLSX file (single line)
-%oct2xls(strcat(output_filename,".xlsx"), full_matrix);
-%xlswrite (strcat(output_filename,".xlsx"), full_matrix);
-
 % write to xlsx file
 xls = xlsopen(strcat(output_filename,".xlsx"), 1);
-xls = oct2xls(full_matrix,xls,1);
+xls = oct2xls(full_matrix,xls,"data");
+xls = oct2xls(atemporal_cmatrix,xls,"sessions");
 xls = xlsclose(xls);
 
 printf(".xlsx salvo. ");
